45f1fcef65e447b7ad5b9d0d75bb9e99bbb7d3a7
Amir Ali <amiraliazimloo123@gmail.com>
2024-11-01 03:02:19 +0330

    feat: implement base config for test running by jest.

---
+++ apps/next-jest/__tests__/page.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+ 
+describe('Config', () => {
+  it('first test', () => {
+ 
+    expect(1).toBeTruthy();
+  })
+})
