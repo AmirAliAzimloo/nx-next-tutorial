d3dde5426c64a1a5955baaf4e51223d041bdf3c7
Amir Ali <amiraliazimloo123@gmail.com>
2024-11-01 14:44:20 +0330

    feat: add the Todo route.

--- apps/next-jest/src/components/home.tsx
+++ apps/next-jest/src/components/home.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+'use client';
+
+import { useRouter } from "next/navigation";
 
 const Home = () => {
+
+  const router = useRouter();
+
   return (
     <div>
       <h1>Home</h1>
       <p data-testid="desc" >This is my description</p>
+
+      <button
+      onClick={()=>{
+        router.push('/todo');
+      }}
+      >
+        Navigate to Todo Page
+      </button>
     </div>
   )
 }
